2d057d41098e72a151495c339dc7b7d4ad8272cf
plucky <py@meitu.com>
2019-10-30 10:56:20 +0800

    添加 package 引用 path 的示例

---
+++ easylib/lib/helper.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+class Helper {
+  Helper._();
+
+  static const releaseMode = bool.fromEnvironment("dart.vm.product");
+
+  static Color hexToColor(String s) {
+    if (s == null ||
+        s.length != 7 ||
+        int.tryParse(s.substring(1, 7), radix: 16) == null) {
+      s = '#999999';
+    }
+    return new Color(int.parse(s.substring(1, 7), radix: 16) + 0xFF000000);
+  }
+
+  static int parseNum(String str) {
+    RegExp reg = new RegExp(r"[0-9]+");
+    Iterable<Match> matches = reg.allMatches(str);
+    return int.tryParse(matches.first.group(0));
+  }
+}
